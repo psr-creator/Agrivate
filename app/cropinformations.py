4aa9923ef7a324b1bf21211e71376656a43c49c0
Shyam Murali <shyammurali23@gmail.com>
2022-04-27 20:05:35 +0530

    added cropinfo

---
+++ app/cropinformations.py
@@ -0,0 +1,69 @@
+#rice,maize,chickpea,kidneybeans,pigeonpeas,mothbeans,mungbean,
+#blackgram,lentil,pomegranate,banana,mango,grapes,watermelon,
+#muskmelon,apple,orange,papaya,coconut,cotton,jute,coffee,
+cropdetails={
+    "rice":{ 
+        "desc":"The cultivated rice plant is an annual grass and grows to about 1.2 metres (4 feet) in height. The leaves are long and flattened and are borne on hollow stems. The fibrous root system is often broad and spreading. The panicle, or inflorescence (flower cluster), is made up of spikelets bearing flowers that produce the fruit, or grain. Varieties differ greatly in the length, shape, and weight of the panicle and the overall productivity of a given plant.",
+         },
+    "maize":{ 
+        "desc":"The corn plant is a tall annual grass with a stout, erect, solid stem. The large narrow leaves have wavy margins and are spaced alternately on opposite sides of the stem. Staminate (male) flowers are borne on the tassel terminating the main axis of the stem. The pistillate (female) inflorescences, which mature to become the edible ears, are spikes with a thickened axis, bearing paired spikelets in longitudinal rows; each row of paired spikelets normally produces two rows of grain. Varieties of yellow and white corn are the most popular as food, though there are varieties with red, blue, pink, and black kernels, often banded, spotted, or striped. Each ear is enclosed by modified leaves called shucks or husks.",
+         },
+    "chickpea":{
+         "desc":"The bushy 60-cm (2-foot) plants bear feathery pinnately compound leaves. The small white or reddish flowers often have distinctive veins in blue or purple and are usually self-pollinated. The yellow-brown or dark green beans are borne one or two to a pod. There are large- and small-seeded varieties.",
+          },
+    "kidneybeans":{ 
+        "desc":"Kidney beans are a variety of the common bean (Phaseolus vulgaris), a legume native to Central America and Mexico.The common bean is an important food crop and major source of protein throughout the world.Used in a variety of traditional dishes, kidney beans are usually eaten well cooked. Raw or improperly cooked kidney beans are toxic, but well-prepared beans can be a healthy component of a well-balanced diet.They come in a variety of colors and patterns, including white, cream, black, red, purple, spotted, striped, and mottled.",
+         },
+    "pigeonpeas":{ 
+        "desc":"Pigeon pea  is one of the most common tropical and subtropical legumes cultivated for its edible seeds. Pigeon pea is fast growing, hardy, widely adaptable, and drought resistant . Because of its drought resistance it can be considered of utmost importance for food security in areas where rainfall is not reliable and droughts are likely to occur . At the end of the dry season, pigeon pea provides green forage of outstanding value when other forages are not available.",
+         },
+    "mothbeans":{ 
+        "desc":"A small bean, very common in India that provides a good source of protein when cooked as a food dish. Pronounced as 'moat' this bean is oblong in shape, small in size and light tan to brown in color. The inner meat, which is golden yellow in color, provides an earthy and somewhat nutty flavor for main dishes, side dishes or snack food (sev) made with this bean. Generally the moth bean is eaten as a sprout or as a dhal (also known as dal) which is a hulled bean. Whole beans which have not been hulled, are also consumed and require soaking prior to cooking. They should remain in water for at least 8 hours after which they can be drained, placed in fresh boiling water, and cooked for 15 minutes or longer. The hulled beans, or dals with the husk removed may not need soaking and require shorter cooking times. Other names for this bean include dew bean, haricot, mat, mot, muth, and papillon.",
+         },
+    "mungbean":{ 
+        "desc":"Mung beans (Vigna radiata) are small, green beans that belong to the legume family.They have been cultivated since ancient times. While native to India, mung beans later spread to China and various parts of Southeast Asia.These beans have a slightly sweet taste and are sold fresh, as sprouts or as dried beans.",
+         },
+    "blackgram":{ 
+        "desc":"Black gram  is an erect, fast-growing annual, herbaceous legume reaching 30-100 cm in height. It has a well-developed taproot and its stems are diffusely branched from the base. Occasionally it has a twining habit and it is generally pubescent. The leaves are trifoliate with ovate leaflets, 4-10 cm long and 2-7 cm wide. The inflorescence is borne at the extremity of a long (up to 18 cm) peduncle and bears yellow, small, papilionaceous flowers. The fruit is a cylindrical, erect pod, 4-7 cm long x 0.5 cm broad. The pod is hairy and has a short hooked beak. It contains 4-10 ellipsoid black or mottled seeds . Many Vigna mungo cultivars exist, each one adapted to specific environmental conditions.",
+         },
+    "lentil":{ 
+        "desc":"The plant varies from 15 to 45 cm (6 to 18 inches) in height and has many long ascending branches. The compound leaves are alternate, with six pairs of oblong-linear leaflets about 15 mm (0.5 inch) long and ending in a spine. Two to four pale blue flowers are borne in the axils of the leaves in June or early July. The small pods are broadly oblong and slightly inflated and contain two seeds the shape of a doubly convex lens and about 4–6 mm (0.17–0.24 inch) in diameter. There are many cultivated varieties of the plant, differing in size, hairiness, and colour of the leaves, flowers, and seeds. The seeds may be more or less compressed in shape and can be white, yellow, orange, tan, green, gray, or dark brown in colour; they are also sometimes mottled or speckled.",
+         },
+    "pomegranate":{ 
+        "desc":"The plant, which may attain 5 or 7 metres (16 or 23 feet) in height, has elliptic to lance-shaped, bright-green leaves about 7.5 cm (3 inches) long. The handsome axillary orange-red flowers are borne toward the ends of the branchlets. The calyx (comprising the sepals) is tubular and persistent and has five to seven lobes; the petals are lance-shaped, inserted between the calyx lobes. The ovary is embedded in the calyx tube and contains several compartments in two series, one above the other.The fruit is the size of a large orange, obscurely six-sided, with a smooth leathery skin that ranges from brownish yellow to red; within, it is divided into several chambers containing many thin transparent arils of reddish, juicy pulp, each surrounding an angular elongated seed.", 
+        },
+    "banana":{ 
+        "desc":"The banana plant is a gigantic herb that springs from an underground stem, or rhizome, to form a false trunk 3–6 metres (10–20 feet) high. This trunk is composed of the basal portions of leaf sheaths and is crowned with a rosette of 10 to 20 oblong to elliptic leaves that sometimes attain a length of 3–3.5 metres (10–11.5 feet) and a breadth of 65 cm (26 inches). A large flower spike, carrying numerous yellowish flowers protected by large purple-red bracts, emerges at the top of the false trunk and bends downward to become bunches of 50 to 150 individual fruits, or fingers. The individual fruits, or bananas, are grouped in clusters, or hands, of 10 to 20. After a plant has fruited, it is cut down to the ground, because each trunk produces only one bunch of fruit. The dead trunk is replaced by others in the form of suckers, or shoots, which arise from the rhizome at roughly six-month intervals. The life of a single rhizome thus continues for many years, and the weaker suckers that it sends up through the soil are periodically pruned, while the stronger ones are allowed to grow into fruit-producing plants.",
+         },
+    "mango":{ 
+        "desc":"The tree is evergreen, often reaching 15–18 metres (50–60 feet) in height and attaining great age. The simple leaves are lanceolate, up to 30 cm (12 inches) long. The flowers—small, pinkish, and fragrant—are borne in large terminal panicles (loose clusters). Some have both stamens and pistils, while others have stamens only. The fruit varies greatly in size and character. Its form is oval, round, heart-shaped, kidney-shaped, or long and slender. The smallest mangoes are no larger than plums, while others may weigh 1.8 to 2.3 kg (4 to 5 pounds). Some varieties are vividly coloured with shades of red and yellow, while others are dull green. The single large seed is flattened, and the flesh that surrounds it is yellow to orange in colour, juicy, and of distinctive sweet-spicy flavour.",
+         },
+    "grapes":{ 
+        "desc":"The grape is usually a woody vine, climbing by means of tendrils (modified branches) and when untrained often reaching a length of 17 metres (56 feet) or more. In arid regions it may form an almost erect shrub. The edible leaves are alternate, palmately lobed, and always tooth-edged. Small greenish flowers, in clusters, precede the fruit, which varies in colour from almost black to green, red, and amber. Botanically, the fruit is a berry, more or less globular, within the juicy pulp of which lie the seeds. In many varieties the fruit develops a whitish powdery coating, or bloom.",
+         },
+    "watermelon":{ 
+        "desc":"The watermelon is a large fruit of a more or less spherical shape. It is usually eaten raw as table dessert. It has a sweet reddish or yellowish flesh. It is a very refreshing food that supplies very few calories. It also provides some vitamins and minerals.The watermelon is a large fruit, up to 25cm of diameter, in some cases weighing up to 15kg. It has an oval or spherical shape and a dark green and smooth rind, sometimes showing irregular areas of a pale green colour. It has a sweet, juicy, refreshing flesh of yellowish or reddish colour, containing multiple black, brown or white pips. Some new varieties, cultivated in the last few years, have changed this classic characteristic, bearing seedless fruits, varieties of yellow pulp or smaller watermelons.The watermelon is generally eaten raw, in slices, quarters or even in small balls.",
+         },
+    "muskmelon":{ 
+        "desc":"Muskmelons vary in shape in size and tend to weigh between 5 and 8 pounds. Most are round to ovate and some will have a slightly squat shape. They have a pronounced light tan netting over their exterior, a characteristic unique to all Muskmelon varieties. Many types of Muskmelon such as this one pictured have very pronounced sutures, giving it a pumpkin-like shape. Its interior contains a central seed cavity which is surrounded by thick, smooth, bright salmon hued flesh. Offering a sweet aroma and flavor, the Muskmelon has a high sugar content with lingering floral notes. When ripe the Muskmelon will have a sweet aroma and will feel weighty for its size. Muskmelons are best consumed within a few weeks of harvesting.",
+         },
+    "apple":{ 
+        "desc":"", },
+    "orange":{ 
+        "desc":"",
+         },
+    "papaya":{ 
+        "desc":"",
+         },
+    "coconut":{ 
+        "desc":"", },
+    "cotton":{ "desc":"",
+     },
+    "jute":{ 
+        "desc":"",
+         },
+    "coffee":{ 
+        "desc":"",
+         },
+
+}
